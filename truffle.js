26b222aa699b58e774a146988b568358e3b643ae
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-04-19 12:19:41 +0200

    single network used

--- truffle.js
+++ truffle.js
@@ -4,13 +4,5 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
-    },
-    gitlab: {
-      // network used only for gitlab tests
-      // here the ganache is avaialbe on trufflesuite-ganache-cli:8545
-      host: 'trufflesuite-ganache-cli',
-      port: 8545,
-      network_id: '*' // Match any network id
     }
-  }
 }
